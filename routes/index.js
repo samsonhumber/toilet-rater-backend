3573b74c94c1717ac7db93dca24745e716e5c5c0
samsonhumber <printerfissh693@gmail.com>
2022-09-28 12:54:08 +0100

    working on crud routes, not finished yet

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import { getReviewsFromToiletName, getReviewsFromUserName } from "../models/getReviews.js";
+import { postToiletReview } from "../models/postReviews.js";
 
 const reviewsRouter = express.Router();
 
@@ -14,6 +15,13 @@ reviewsRouter.get("/toiletreviews", async function (req, res) {
 reviewsRouter.get("/userreviews", async function (req, res) {
   const userName = String(req.query.username);
   const result = await getReviewsFromUserName(userName);
+  const responseObject = { success: true, payload: result };
+  res.json(responseObject);
+});
+
+reviewsRouter.post("/review", async function (req, res) {
+  const newReview = req.body;
+  const result = await postToiletReview(newReview);
   const responseObject = { success: true, payload: result };
   res.json(responseObject);
 });
